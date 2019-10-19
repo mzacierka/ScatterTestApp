1917118daa1158d20a9d5073f3179443d8823680
KartoffelMann <auriemmat6@students.rowan.edu>
2019-10-19 03:22:21 -0400

    Improved styling

--- FlaskAPP/endpoints/Settings/routes.py
+++ FlaskAPP/endpoints/Settings/routes.py
@@ -1,12 +1,10 @@
 import os
-from flask import Flask, Blueprint, send_from_directory, render_template, redirect, url_for, flash, request, send_file
+from flask import Flask, Blueprint, render_template, redirect, url_for, flash, request, send_file
 from werkzeug.utils import secure_filename
 from flask_login import login_required, current_user
 from FlaskAPP.config import Config
 from FlaskAPP.endpoints.Login.forms import LoginForm
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from FlaskAPP.models.jsonfiles import JSONFiles
 from FlaskAPP import db
 from io import BytesIO
@@ -14,6 +12,7 @@ from io import BytesIO
 
 settings = Blueprint('settings', __name__)
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = {'json'}
 
 # The upload feature. Routes to uploaded_file upon successful completion
 @login_required
@@ -24,22 +23,42 @@ def show():
     else:
         return render_template('Login/login.html', form=LoginForm())
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 # Landing page upon successful upload, needs to be changed
 @login_required
 @settings.route('/settings/upload/uploadFile', methods=['POST'])
 def upload_file():
 
-    file = request.files['file']
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect('/settings')
+        file = request.files['file']
+        # check if file exists
+        if file.filename == '':
+            flash('No selected file')
+            return redirect('/settings')
+        # check if file is an allowed extension
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # Send to database        
+            db.session.rollback()
+            newFile = JSONFiles(name=file.filename, data=file.read())
+            db.session.add(newFile)
+            db.session.commit()
+        
+            flash('Upload Successful')
 
-    db.session.rollback()
-    newFile = JSONFiles(name=file.filename, data=file.read())
-    db.session.add(newFile)
-    db.session.commit()
+            return redirect('/settings')
+        # exit failure
+        flash('File not correct extension')
+        return redirect('/settings')
 
-    if current_user.is_authenticated:
-        return render_template('Settings/settings.html', user=current_user, file_data = JSONFiles.query.order_by(JSONFiles.name).all())
-    else:
-        return render_template('Login/login.html', form=LoginForm())
+    
     
 # Displays JSON downloads
 @login_required
