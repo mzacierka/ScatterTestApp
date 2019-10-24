2e989b447a9c1b5f1fabb45c56de329e6c3d4111
= <=>
2019-10-24 12:13:21 -0400

    Formatting fixes

--- FlaskAPP/endpoints/Data/routes.py
+++ FlaskAPP/endpoints/Data/routes.py
@@ -7,17 +7,22 @@ from FlaskAPP import ma
 import os
 import json
 import requests
+
 from io import BytesIO
+
 
 class QuestionSchema(ma.ModelSchema):
     class Meta:
         model = Questions
 
+
 class JSONFileSchema(ma.ModelSchema):
     class Meta:
         model = JSONFiles
 
+
 data = Blueprint('data', __name__)
+
 
 @data.route("/data/upload_patient_test_data", methods=['POST'])
 def upload_patient_test_data():
@@ -31,10 +36,12 @@ def upload_patient_test_data():
     print("This should work")
     return "Great job!"
 
+
 @data.route('/data/download/<filename>')
 def download_test(filename):
     file_data = JSONFiles.query.filter_by(name=filename).first()
     return send_file(BytesIO(file_data.data), attachment_filename=filename, as_attachment=True)
+
 
 @data.route('/data/download/getTestList')
 def getList():
@@ -43,12 +50,14 @@ def getList():
     output = file_schema.dump(fileList)
     return jsonify(output)
 
+
 @data.route("/data/download_test_info", methods=['POST', 'GET'])
 def download_test_info():
     # pull from database
     # downloadFile = TestData.query.filter_by("DoctorID").all()
     return "Great job!"
-    #add code that will grab the json stored in the database and push it to the app
+    # add code that will grab the json stored in the database and push it to the app
+
 
 @data.route("/data/upload_patient_questionnaire_answers", methods=['POST'])
 def upload_patient_questionnaire_answers():
@@ -59,8 +68,10 @@ def upload_patient_questionnaire_answers():
     print(questionnaire_answers)
     return "Great job!"
 
-	#make sure to perform error checking - this endpoint is open to anyone
-	
+
+# make sure to perform error checking - this endpoint is open to anyone
+
+
 @data.route("/data/download_questions", methods=['POST', 'GET'])
 def download_questions():
 
--- FlaskAPP/models/jsonfiles.py
+++ FlaskAPP/models/jsonfiles.py
@@ -1,5 +1,6 @@
 from FlaskAPP import db
 
+
 class JSONFiles(db.Model):
     name = db.Column(db.String(50), primary_key=True)
     data = db.Column(db.LargeBinary)
