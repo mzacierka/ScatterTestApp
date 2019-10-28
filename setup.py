2d46469d141ce011b85d28e5c6655d0f3047ee79
KartoffelMann <auriemmat6@students.rowan.edu>
2019-10-28 19:08:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'marshmallow-sqlalchemy',
         'flask-sqlalchemy',
         'flask-security',
-        'mysqlclient',
+        'PyMySQL',
         'wtforms',
         'requests',
         'XlsxWriter'
