7e5840686ee0da72a9792c6386c8e4c3cb13d530
KartoffelMann <auriemmat6@students.rowan.edu>
2019-10-28 22:57:50 -0400

    hotfix

--- FlaskAPP/models/Questions.py
+++
@@ -1,8 +0,0 @@
-from FlaskAPP import db
-
-
-class Questions(db.Model):
-    QuestionID = db.Column(db.Integer, primary_key=True)
-    QuestionType = db.Column(db.Integer)
-    PossibleAnswers = db.Column(db.String)
-    Question = db.Column(db.String)
